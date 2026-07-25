80591b6fc104f1586c1fe0ae2545ca5a5d163c4b
Abdul Rafay <abdulrafaywattoo@gmail.com>
2026-07-25 19:05:59 +0800

    Disable ESLint blocking on production build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  eslint: {
+  ignoreDuringBuilds: true,
+},
   /* config options here */
 };
+
+
 
 export default nextConfig;
